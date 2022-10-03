6fb4e6e889389d95621d3a313cc97952fbf508ec
Aakash Choudhary <48271611+achoudhary9009@users.noreply.github.com>
2022-10-03 10:43:10 +0200

    Initial commit

---
+++ server/src/main/scala/com/lunatech/goldenalgo/onboarding/Controller.scala
@@ -0,0 +1,24 @@
+package com.lunatech.goldenalgo.onboarding
+
+import akka.http.scaladsl.server.Directives._
+import scala.concurrent.ExecutionContext
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.model._
+import io.circe.syntax._
+import akka.http.scaladsl.model.headers.RawHeader
+
+class Controller()(implicit val ec: ExecutionContext) {
+
+  // TODO: replace this hard-coded value by CRUD operations
+  val recipe1 = Recipe("recipe1-id", "recipe1-name", Seq("ingredient1", "ingredient2"), Seq("instruction1", "instruction2"))
+
+  val recipes = path("recipes") {
+    get {
+      respondWithHeader(RawHeader("Access-Control-Allow-Origin", "*")) {
+        complete(HttpEntity(ContentTypes.`application/json`, Seq(recipe1).asJson.noSpaces))
+      }
+    }
+  }
+
+  val routes: Route = recipes
+}
